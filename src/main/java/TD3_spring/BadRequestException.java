895671707f19cf750fef41eae439ca2f92163d48
Toavandr <hei.toavina.2@gmail.com>
2026-03-25 19:21:08 +0300

    feat:add file badrequest for this exercice

---
+++ src/main/java/TD3_spring/BadRequestException.java
@@ -0,0 +1,9 @@
+package com.example.TD3_spring;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
